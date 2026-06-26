54087a1cc4ed5d07b5292047deba33abdcc82b09
prajwal0405 <kawthekarprajwal@gmail.com>
2026-06-26 12:36:10 +0530

    Set Vite base path for nested deployment under dist folder

--- landing-pages/Teacher-Training-Program/react-project/vite.config.js
+++ landing-pages/Teacher-Training-Program/react-project/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/landing-pages/Teacher-Training-Program/react-project/dist/',
   plugins: [react()],
   server: {
     proxy: {
