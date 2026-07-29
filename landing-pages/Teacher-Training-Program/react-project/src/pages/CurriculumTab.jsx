690e2733ac089fc436e997291ecbf51b7afafc41
Prajwal Kavathekar <kawthekarprajwal@gmail.com>
2026-07-29 20:28:21 +0530

    Revert "bug solved login page"

--- landing-pages/Teacher-Training-Program/react-project/src/pages/CurriculumTab.jsx
+++ landing-pages/Teacher-Training-Program/react-project/src/pages/CurriculumTab.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { S, StatusBadge } from "../components/Shared";
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://teacher-training-portal.onrender.com";
 
 function CurriculumTab({ user }) {
   const [assignments, setAssignments] = useState([]);
